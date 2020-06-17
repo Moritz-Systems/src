1ae686e129c66cd0804e3cc049c1f8f385150a1a
Kamil Rytarowski <n54@gmx.com>
2020-06-17 19:09:21 +0200

    fix

--- sys/kern/zone.c
+++ sys/kern/zone.c
@@ -238,8 +238,6 @@ extern kauth_cred_t	cred0;
     kauth_cred_setgroups(cr, ga, gc, 0, UIO_SYSSPACE)
 #define crgetsid(cr, i) (NULL)
 
-typedef struct kauth_cred cred_t;
-
 static __inline int
 groupmember(gid_t gid, cred_t *cr)
 {
