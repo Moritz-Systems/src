9f774cab45455a274c53d852602f790dda523631
Kamil Rytarowski <n54@gmx.com>
2020-06-20 02:11:59 +0200

    fix

--- sys/sys/sol_pool.h
+++ sys/sys/sol_pool.h
@@ -23,8 +23,8 @@
  * Use is subject to license terms.
  */
 
-#ifndef	_SYS_POOL_H
-#define	_SYS_POOL_H
+#ifndef	_SYS_XOOL_H
+#define	_SYS_XOOL_H
 
 #include <sys/types.h>
 #include <sys/time.h>
@@ -36,106 +36,106 @@
 extern "C" {
 #endif
 
-#define	POOL_DEFAULT		0		/* default pool's ID */
-#define	POOL_MAXID		999999		/* maximum possible pool ID */
-#define	POOL_INVALID		-1
+#define	XOOL_DEFAULT		0		/* default xool's ID */
+#define	XOOL_MAXID		999999		/* maximum possible xool ID */
+#define	XOOL_INVALID		-1
 
-/* pools states */
-#define	POOL_DISABLED		0		/* pools enabled */
-#define	POOL_ENABLED		1		/* pools disabled */
+/* xools states */
+#define	XOOL_DISABLED		0		/* xools enabled */
+#define	XOOL_ENABLED		1		/* xools disabled */
 
 #ifdef	_KERNEL
 
-struct pool_pset;
+struct xool_pset;
 
-typedef struct pool {
-	poolid_t		pool_id;	/* pool ID */
-	uint32_t		pool_ref;	/* # of procs in this pool */
-	list_node_t		pool_link;	/* links to next/prev pools */
-	nvlist_t		*pool_props;	/* pool properties */
-	struct pool_pset	*pool_pset;	/* pool's pset */
-} pool_t;
-
-/*
- * Flags for pool_do_bind
- */
-#define	POOL_BIND_PSET	0x00000001
-#define	POOL_BIND_ALL	POOL_BIND_PSET
+typedef struct xool {
+	xoolid_t		xool_id;	/* xool ID */
+	uint32_t		xool_ref;	/* # of procs in this xool */
+	list_node_t		xool_link;	/* links to next/prev xools */
+	nvlist_t		*xool_props;	/* xool properties */
+	struct xool_pset	*xool_pset;	/* xool's pset */
+} xool_t;
 
 /*
- * Result codes for pool_get_class()
+ * Flags for xool_do_bind
  */
-#define	POOL_CLASS_UNSET	-1		/* no scheduling class set */
-#define	POOL_CLASS_INVAL	-2		/* class is invalid */
+#define	XOOL_BIND_PSET	0x00000001
+#define	XOOL_BIND_ALL	XOOL_BIND_PSET
 
-extern int	pool_count;	/* current number of pools */
-extern pool_t	*pool_default;	/* default pool pointer */
-extern int	pool_state;	/* pools state -- enabled/disabled */
-extern void	*pool_buf;	/* last state snapshot */
-extern size_t	pool_bufsz;	/* size of pool_buf */
+/*
+ * Result codes for xool_get_class()
+ */
+#define	XOOL_CLASS_UNSET	-1		/* no scheduling class set */
+#define	XOOL_CLASS_INVAL	-2		/* class is invalid */
+
+extern int	xool_count;	/* current number of xools */
+extern xool_t	*xool_default;	/* default xool pointer */
+extern int	xool_state;	/* xools state -- enabled/disabled */
+extern void	*xool_buf;	/* last state snapshot */
+extern size_t	xool_bufsz;	/* size of xool_buf */
 
 /*
  * Lookup routines
  */
-extern pool_t	*pool_lookup_pool_by_id(poolid_t);
-extern pool_t	*pool_lookup_pool_by_name(char *);
-extern pool_t	*pool_lookup_pool_by_pset(int);
+extern xool_t	*xool_lookup_xool_by_id(xoolid_t);
+extern xool_t	*xool_lookup_xool_by_name(char *);
+extern xool_t	*xool_lookup_xool_by_pset(int);
 
 /*
  * Configuration routines
  */
-extern void 	pool_init(void);
-extern int	pool_status(int);
-extern int	pool_create(int, int, id_t *);
-extern int	pool_destroy(int, int, id_t);
-extern int	pool_transfer(int, id_t, id_t, uint64_t);
-extern int	pool_assoc(poolid_t, int, id_t);
-extern int	pool_dissoc(poolid_t, int);
-extern int	pool_bind(poolid_t, idtype_t, id_t);
-extern id_t	pool_get_class(pool_t *);
-extern int	pool_do_bind(pool_t *, idtype_t, id_t, int);
-extern int	pool_query_binding(idtype_t, id_t, id_t *);
-extern int	pool_xtransfer(int, id_t, id_t, uint_t, id_t *);
-extern int	pool_pack_conf(void *, size_t, size_t *);
-extern int	pool_propput(int, int, id_t, nvpair_t *);
-extern int	pool_proprm(int, int, id_t, char *);
-extern int	pool_propget(char *, int, int, id_t, nvlist_t **);
-extern int	pool_commit(int);
-extern void	pool_get_name(pool_t *, char **);
+extern void 	xool_init(void);
+extern int	xool_status(int);
+extern int	xool_create(int, int, id_t *);
+extern int	xool_destroy(int, int, id_t);
+extern int	xool_transfer(int, id_t, id_t, uint64_t);
+extern int	xool_assoc(xoolid_t, int, id_t);
+extern int	xool_dissoc(xoolid_t, int);
+extern int	xool_bind(xoolid_t, idtype_t, id_t);
+extern id_t	xool_get_class(xool_t *);
+extern int	xool_do_bind(xool_t *, idtype_t, id_t, int);
+extern int	xool_query_binding(idtype_t, id_t, id_t *);
+extern int	xool_xtransfer(int, id_t, id_t, uint_t, id_t *);
+extern int	xool_pack_conf(void *, size_t, size_t *);
+extern int	xool_propput(int, int, id_t, nvpair_t *);
+extern int	xool_proprm(int, int, id_t, char *);
+extern int	xool_propget(char *, int, int, id_t, nvlist_t **);
+extern int	xool_commit(int);
+extern void	xool_get_name(xool_t *, char **);
 
 /*
  * Synchronization routines
  */
-extern void	pool_lock(void);
-extern int	pool_lock_intr(void);
-extern int	pool_lock_held(void);
-extern void	pool_unlock(void);
-extern void	pool_barrier_enter(void);
-extern void	pool_barrier_exit(void);
+extern void	xool_lock(void);
+extern int	xool_lock_intr(void);
+extern int	xool_lock_held(void);
+extern void	xool_unlock(void);
+extern void	xool_barrier_enter(void);
+extern void	xool_barrier_exit(void);
 
 typedef enum {
-	POOL_E_ENABLE,
-	POOL_E_DISABLE,
-	POOL_E_CHANGE,
-} pool_event_t;
+	XOOL_E_ENABLE,
+	XOOL_E_DISABLE,
+	XOOL_E_CHANGE,
+} xool_event_t;
 
-typedef void pool_event_cb_func_t(pool_event_t, poolid_t,  void *);
+typedef void xool_event_cb_func_t(xool_event_t, xoolid_t,  void *);
 
-typedef struct pool_event_cb {
-	pool_event_cb_func_t	*pec_func;
+typedef struct xool_event_cb {
+	xool_event_cb_func_t	*pec_func;
 	void			*pec_arg;
 	list_node_t		pec_list;
-} pool_event_cb_t;
+} xool_event_cb_t;
 
 /*
- * Routines used to register interest in changes in cpu pools.
+ * Routines used to register interest in changes in cpu xools.
  */
-extern void pool_event_cb_register(pool_event_cb_t *);
-extern void pool_event_cb_unregister(pool_event_cb_t *);
+extern void xool_event_cb_register(xool_event_cb_t *);
+extern void xool_event_cb_unregister(xool_event_cb_t *);
 #endif	/* _KERNEL */
 
 #ifdef	__cplusplus
 }
 #endif
 
-#endif	/* _SYS_POOL_H */
+#endif	/* _SYS_XOOL_H */
