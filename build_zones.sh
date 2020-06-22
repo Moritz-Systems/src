c62f3b8082b2a295f0b619f9526f30d48697026a
Kamil Rytarowski <n54@gmx.com>
2020-06-22 17:18:56 +0200

    id

--- build_zones.sh
+++ build_zones.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #./build.sh -V MAKECONF=/dev/null -V MKLLVMRT=no -N0 -j8 -U -u -O /public/netbsd-zones tools
-./build.sh -V MAKECONF=/dev/null -V MKLLVMRT=no -N0 -j8 -U -u -O /public/netbsd-zones kernel=GENERIC
+./build.sh -V MAKECONF=/dev/null -V MKLLVMRT=no -N0 -j1 -U -u -O /public/netbsd-zones kernel=GENERIC
