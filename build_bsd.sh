4d9f90b08ffe04c0e04855e3ef5d3dd077d5a169
Kamil Rytarowski <n54@gmx.com>
2020-07-16 23:57:28 +0200

    fix

---
+++ build_bsd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#rm -rf /public/netbsd-zones
+#./build.sh -V MAKECONF=/dev/null -V MKLLVMRT=no -N0 -j8 -U -u -O /public/netbsd-zones tools
+./build.sh -V MAKECONF=/dev/null -V MKLLVMRT=no -N0 -j1 -U -u -O /public/netbsd-zones kernel=GENERIC
